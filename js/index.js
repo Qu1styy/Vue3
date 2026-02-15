642dd13d8b8f4d584995ebeebba2bdbf3125756d
qu1sty <ameserakov304@gmail.com>
2026-02-15 23:01:50 +0700

    commit 4

--- js/index.js
+++ js/index.js
@@ -1,16 +1,37 @@
 Vue.component('create-task', {
+    data() {
+        return {
+            title: '',
+            description: '',
+            deadline: ''
+        }
+    },
+    methods:{
+        create(){
+            if (!this.title || !this.deadline) return
+            const now = new Date().toLocaleString('ru-RU')
 
-    id,
-    title,
-    description,
-    createdAt,
-    updatedAt,
-    deadline,
-    deadlineRaw,
-    status: 'todo',
-    returnReason: null,
-    isCompletedInTime: false
+            const task = {
+                id: Date.now(),
+                title: this.title,
+                description: this.description,
+                createdAt: now,
+                updatedAt: now,
+                deadline: new Date(this.deadline).toLocaleString('ru-RU'),
+                deadlineRaw: this.deadline,
+                status: 'todo',
+                returnReason: null,
+                isCompletedInTime: false
+            }
 
+            this.$emit('create', task);
+
+            this.title = ''
+            this.description = ''
+            this.deadline = ''
+
+        }
+    },
 })
 
 Vue.component('task-card', {
@@ -25,7 +46,11 @@ Vue.component('task-card', {
         moveTask(task) {
             this.$emit('move-forward', this.task)
         },
-        moveBack(){},
+        moveBack(){
+            const reason = prompt('Please indicate the reason for return');
+            if(!reason) return
+            this.$emit('move-back', { task: this.task, reason });
+        }
     }
 })
 
@@ -42,6 +67,86 @@ new Vue({
     methods: {
         addTask(task){
             this.columns.todo.push(task)
+            this.save()
+        },
+
+        editTask(task){
+
+            const isOverdue =
+                task.status !== 'done' &&
+                new Date() > new Date(task.deadlineRaw)
+
+            if (isOverdue) {
+                alert('The date of overdue notes cannot be changed')
+                return
+            }
+
+            const title = prompt('New title', task.title)
+            if (title) {
+                task.title = title
+            }
+
+            const newDeadline = prompt('New deadline', task.deadlineRaw)
+            if (newDeadline) {
+                task.deadlineRaw = newDeadline
+                task.deadline = new Date(newDeadline).toLocaleString('ru-RU')
+            }
+
+            task.updatedAt = new Date().toLocaleString('ru-RU')
+            this.save()
+        },
+
+        deleteTask(task) {
+            this.columns[task.status] =
+                this.columns[task.status].filter(t => t.id !== task.id)
+            this.save()
+        },
+
+        finishTask(task) {
+            const deadline = new Date(task.deadlineRaw)
+            const now = new Date()
+            task.isCompletedInTime = now <= deadline
+            task.returnReason = null
+            this.move(task, 'testing', 'done')
+        },
+
+        moveForward(task) {
+            if (task.status === 'todo'){
+                this.move(task, 'todo', 'inProgress')
+            }
+            else if (task.status === 'inProgress'){
+                this.move(task, 'inProgress', 'testing')
+            }
+            else if (task.status === 'testing'){
+                this.finishTask(task)
+            }
+        },
+
+        moveBack({ task, reason }) {
+            task.returnReason = reason
+            this.move(task, 'testing', 'inProgress')
+        },
+
+        move(task, from, to) {
+            this.columns[from] = this.columns[from].filter(t => t.id !== task.id)
+            task.status = to
+            task.updatedAt = new Date().toLocaleString('ru-RU')
+            this.columns[to].push(task)
+            this.save()
+        },
+
+        save() {
+            localStorage.setItem('kanban', JSON.stringify(this.columns))
+        },
+
+        load() {
+            const data = localStorage.getItem('kanban')
+            if (data) this.columns = JSON.parse(data)
         }
+    },
+
+    mounted() {
+        this.load()
     }
+
 })
